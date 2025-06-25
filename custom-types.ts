949dc7c28862a9ce383dc41c433d6b01b8e7e7cb
Tunde Sanusi <52820977+tuhamworld@users.noreply.github.com>
2025-06-25 17:43:10 +0100

    chore: custom types

---
+++ custom-types.ts
@@ -0,0 +1,19 @@
+type Person = {
+    name: string
+    age: number
+    isStudent: boolean
+}
+
+
+
+let person1: Person = {
+    name: "Joe",
+    age: 42,
+    isStudent: true
+}
+
+let person2: Person = {
+    name: "Jill",
+    age: 66,
+    isStudent: false
+}
